35873d5da0931a5def176946f7ef31446f828588
moraispgsi <moraispgsi@gmail.com>
2017-12-02 03:35:10 +0000

    Server and client separation

---
+++ Client/terminal-devices/base-client.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Ricardo Morais on 03/11/2017.
+ */
+let io = require('socket.io-client');
+
+class Client {
+    constructor(MAC, IP, port, cb) {
+            this.MAC = MAC;
+
+        let requestTimeout;
+        let handle = ()=> {
+            console.log("The request for authentication timed out. Trying again in 5 sec");
+            setTimeout(()=>{
+                socket.emit('auth', { MAC: MAC });
+                requestTimeout = setTimeout(handle, 5000);
+            }, 1000 * 5);
+        };
+
+        let socket = io(`https://${IP}:${port}`,  {rejectUnauthorized: false});
+        socket.on('auth-refused', () => {
+            console.log(`MAC ${this.MAC}: authorization refused trying again in 5 sec`);
+            clearTimeout(requestTimeout);
+            setTimeout(()=>{
+                socket.connect();
+                socket.emit('auth', { MAC: MAC });
+                requestTimeout = setTimeout(handle, 5000);
+            }, 1000 * 5);
+        });
+        socket.on('auth-accepted', () => {
+            console.log(`MAC ${this.MAC}: authorization accepted`);
+            clearTimeout(requestTimeout);
+            socket.off('auth-refused');
+            socket.off('auth-accepted');
+            cb(socket);
+        });
+        //Start by emitting an auth request
+        socket.emit('auth', { MAC: MAC });
+        requestTimeout = setTimeout(handle, 5000);
+
+        console.log(`MAC ${this.MAC}: attempting to gain authorization`);
+    };
+}
+
+module.exports = Client;
